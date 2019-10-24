5464be08daa8b3b952bb0cdb775bb1446c28e100
Nia Ibrahim <nibrahi3@uncc.edu>
2019-10-24 11:39:21 -0400

    Initial commit

---
+++ app/src/main/java/com/example/hw06/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.hw06;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("My Profile");
+    }
+}
