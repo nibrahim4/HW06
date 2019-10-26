b3a53c6148b6723e6ded6c62b5ec723eb2cc2074
Nia Ibrahim <nibrahi3@uncc.edu>
2019-10-25 22:17:56 -0400

    Navigating to the Select Avatar Fragment implementation.

--- app/src/main/java/com/example/hw06/MainActivity.java
+++ app/src/main/java/com/example/hw06/MainActivity.java
@@ -4,13 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MyProfileFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle("My Profile");
+
+           MyProfileFragment profileFragment = new MyProfileFragment();
+
+           getFragmentManager().beginTransaction()
+                .add(R.id.container, profileFragment, "myProfile")
+                .commit();
+
+    }
+
+    @Override
+    public void goToNextFragment() {
+           getFragmentManager().beginTransaction()
+                .replace(R.id.container, new SelectAvatarFragment(), "selectAvatar")
+                .commit();
 
     }
 }
